803364ab949d5c65e440100e534a67f2eec3a5a2
mcmoodoo <rashid.makhmud@gmail.com>
2026-03-05 11:03:43 -0600

    eks cluster tf added

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,19 @@ output "aws_region" {
   value       = var.aws_region
 }
 
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster."
+  value       = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "Endpoint for the EKS cluster."
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "eks_cluster_oidc_issuer" {
+  description = "OIDC issuer URL for the EKS cluster."
+  value       = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+
